d1d9474b25bf8f2dfee64cafe468480e960a34ce
Marek Goczoł <marekgoczol@gmail.com>
2014-06-02 18:28:49 +0200

    home form

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@ $(function(){
 	var Vax = {};
 
 	Vax.VARS = {
-		NAV: $('.jsNav')
+		NAV: $('.jsNav'),
+		HOME_CAROUSEL: $('.jsCarousel'),
+		DROPDOWN: $('.jsDropdown')
 	};
 
 	Vax.navigation = function($elem) {
@@ -15,13 +17,39 @@ $(function(){
 			e.preventDefault();
 			$list.toggleClass('open');
 		});
-	}
+	};
+
+	Vax.homeCarousel = function($elem) {
+		$elem.cycle({
+			slides: '>div',
+			next: '.next',
+			prev: '.prev',
+			pager: '.jsPager'
+		});
+	};
+
+	Vax.dropdown = function($elem) {
+		$elem.each(function(i,v){
+			var $this = $(v);
+			console.log($this);
+
+			$this.selectbox({
+				effect: "fade"
+			});
+		});
+	};
 
 	Vax.init = function() {
 		var $window = $(window);
 
         if(this.VARS.NAV.length) {
             this.navigation(this.VARS.NAV);
+        }
+        if(this.VARS.HOME_CAROUSEL.length) {
+        	this.homeCarousel(this.VARS.HOME_CAROUSEL);
+        }
+        if(this.VARS.DROPDOWN.length) {
+        	this.dropdown(this.VARS.DROPDOWN);
         }
 
 	};
